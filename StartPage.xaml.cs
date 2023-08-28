ce9cc4562e9872e72dcb6b877c91f027b3875ce8
Snowkai <34677392+Snowkai@users.noreply.github.com>
2023-08-28 15:30:27 +0300

    secon day half

--- StartPage.xaml.cs
+++ StartPage.xaml.cs
@@ -10,14 +10,14 @@ public partial class StartPage : ContentPage
 	}
 
 	List<Profile> profiles = new List<Profile>();
-
+	Dictionary<string, Button> buttons = new Dictionary<string, Button>();
     async void CreateForm(object sender, System.EventArgs e)
 	{
 		var name = await DisplayPromptAsync("Create profile", "Enter profile name:");
 		if (name != null) {
 			Profile profile = new Profile(name);
 			profiles.Add(profile);
-			Button button = new Button { 
+			Button button = new Button() {				
 				Text = name,
                 CornerRadius = 10,
                 VerticalOptions = LayoutOptions.Start,
@@ -29,6 +29,7 @@ public partial class StartPage : ContentPage
             };
             button_set.Children.Add(button);
             button.Clicked += NextPage;
+			buttons.Add(name, button);
 		}
 		else
 		{
@@ -38,5 +39,16 @@ public partial class StartPage : ContentPage
 	async private void NextPage(object sender, System.EventArgs e)
 	{
 		await Navigation.PushAsync(new MainPage());
+	}
+	async private void DeleteProfile(object sender, System.EventArgs e)
+	{
+		string[] names = new string[profiles.Count];
+		for(int i = 0; i < names.Length; i++)
+		{
+			names[i] = profiles[i].Name;
+		}
+        var name = await DisplayActionSheet("Delete Profile", "Cancel", "Delete", names);
+		profiles.Remove(profiles.Find(item => item.Name == name));
+		buttons.Remove(name);
 	}
 }
